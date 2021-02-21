7a20e0642f731dafe33de29d4c2558e1a240c496
MrBoombastic <18630357+MrBoombastic@users.noreply.github.com>
2021-02-21 20:07:45 +0100

    help poprawki

--- commands/help.js
+++ commands/help.js
@@ -6,9 +6,9 @@ module.exports = {
             .setColor('GREEN')
             .setTitle('Pomoc bota HRejterzy')
             .addField('Prefix:', config.prefix, true)
-            .addField('Komendy:', `\`${config.prefix}random\` - Pokazuje losowy cytat HRejterów.` +
-                `\n\`${config.prefix}stats\` - Pokazuje statystyki bota.` +
-                `\n\`${config.prefix}help\` - Wyświetla niniejszą pomoc.`, true)
+            .addField('Komendy:', `\`random\` - Pokazuje losowy cytat HRejterów.` +
+                `\n\`stats\` - Pokazuje statystyki bota.` +
+                `\n\`help\` - Wyświetla niniejszą pomoc.`, true)
             .setURL("https://github.com/MrBoombastic/HRejterzy-BOT")
             .setTimestamp();
 
